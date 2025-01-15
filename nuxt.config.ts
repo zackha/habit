b6b659bd5fca7a6e8581eaaa00c677b19638b413
Sefa Bulak <zckhtln@icloud.com>
2025-01-16 00:42:00 +0300

    added hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
+  modules: ['@nuxthub/core'],
+  hub: {
+    database: true,
+  },
 });

8cce89dc2ec7667f5e82835a733ad236fd796f88
Sefa Bulak <zckhtln@icloud.com>
2025-01-17 18:32:19 +0300

    some fix

--- server/api/habits/index.post.ts
+++ server/api/habits/index.post.ts
@@ -1,10 +1,12 @@
 import { useValidatedBody, z } from 'h3-zod';
 
+const HabitSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
 export default eventHandler(async event => {
-  const { title, description } = await useValidatedBody(event, {
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
-  });
+  const { title, description } = await useValidatedBody(event, HabitSchema);
 
   const { user } = await requireUserSession(event);
 

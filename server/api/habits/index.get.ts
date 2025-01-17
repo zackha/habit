7a06dd21b7482fecefc5a267fae99faf251b190b
Sefa Bulak <zckhtln@icloud.com>
2025-01-17 17:06:41 +0300

    Update index.get.ts

--- server/api/habits/index.get.ts
+++ server/api/habits/index.get.ts
@@ -3,7 +3,6 @@ import { eq } from 'drizzle-orm';
 export default eventHandler(async event => {
   const { user } = await requireUserSession(event);
 
-  // List todos for the current user
   const habits = await useDB().select().from(tables.habits).where(eq(tables.habits.userId, user.id)).all();
 
   return habits as Habit[];

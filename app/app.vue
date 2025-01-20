de2174f1c11b634cb177b59dafbe3e32d702972d
Sefa Bulak <zckhtln@icloud.com>
2025-01-20 10:41:45 +0300

    added empty habit

--- app/app.vue
+++ app/app.vue
@@ -67,7 +67,6 @@ const items = [
 ];
 
 const createHabitModal = ref(false);
-const openHabitModal = ref(false);
 </script>
 
 <template>
@@ -115,6 +114,19 @@ const openHabitModal = ref(false);
         </div>
       </div>
       <HabitCard v-for="habit in habits" :key="habit.id" :habit="habit" />
+      <div v-if="emptyHabits" class="flex flex-col items-center justify-center text-center rounded-2xl border border-neutral-800 mx-3 mb-3 bg-neutral-950 relative overflow-hidden">
+        <div class="absolute flex flex-col gap-0.5">
+          <div v-for="row in 14" :key="row" class="flex w-full gap-0.5">
+            <div v-for="col in 27" :key="col" class="w-2.5 h-2.5 rounded-sm bg-neutral-600/10"></div>
+          </div>
+        </div>
+        <div class="absolute w-full h-full bg-gradient-to-t from-neutral-950"></div>
+        <div class="z-10 flex flex-col items-center justify-center p-6 gap-2">
+          <UButton class="mb-2" icon="i-heroicons-plus-16-solid" @click="createHabitModal = true" size="xl" variant="solid" :ui="{ rounded: 'rounded-full' }" />
+          <div class="font-medium">No habit found</div>
+          <div class="text-xs text-neutral-400">Create a new habit to track your progress</div>
+        </div>
+      </div>
     </div>
     <div class="flex flex-col items-center text-xs text-neutral-600">
       <div>Habit v{{ config.public.version }}</div>

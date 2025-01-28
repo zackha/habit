1882d95b173f62c95a6b62db5247519a0fcd6b29
Sefa Bulak <zckhtln@icloud.com>
2025-01-28 21:47:30 +0300

    fix: Mobile UI

--- app/app.vue
+++ app/app.vue
@@ -32,7 +32,9 @@ useSeoMeta({
       <Card v-if="user">
         <div class="relative z-10">
           <ProfileHeader />
-          <HabitCard v-for="habit in habits" :key="habit.id" :habit="habit" />
+          <div class="max-h-[calc(100vh-18.875rem)] overflow-y-auto">
+            <HabitCard v-for="habit in habits" :key="habit.id" :habit="habit" />
+          </div>
           <EmptyHabits v-if="emptyHabits" />
         </div>
       </Card>

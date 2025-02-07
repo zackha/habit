3c8339ebbeb54d2c6efa2d70f90d9cb2bbdf7e42
Sefa Bulak <zckhtln@icloud.com>
2025-02-07 16:12:50 +0300

    Delete index.get.ts

--- server/api/users/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { eq } from 'drizzle-orm';
-
-export default eventHandler(async event => {
-  const { user: requestUser } = await requireUserSession(event);
-
-  const user = await useDB().select().from(tables.users).where(eq(tables.users.id, requestUser.id)).limit(1).get();
-
-  return user;
-});
